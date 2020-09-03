232b31296d2904dce6431f7bc0b89d90fc042916
AhsanZX97 <ahsan97@hotmail.co.uk>
2020-09-03 14:44:38 +0100

    resources can be deleted now

--- ProgrammingJournal/urls.py
+++ ProgrammingJournal/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Journal.views import resources, addPage, addResource
+from Journal.views import resources, addPage, addResource, delResource
 
 
 urlpatterns = [
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', resources),
     path('add/', addPage),
     path('addResource/', addResource),
+    path('delResource/<int:i>/', delResource),
 ]
